1a6151fb57387324505a6c0f6dd91295f0e7184a
yazilimteknolojileriakademisi <85497372+yazilimteknolojileriakademisi@users.noreply.github.com>
2024-03-20 17:15:46 +0300

    Update multiple_linear_regression.py

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -7,7 +7,6 @@
 # 
 # Linear Regression veriler arasında var olan korelasyonu(ilişkiyi) kullanarak yeni gelecek verileri tahmin etme modelidir. Burada makine öğrenimi bize veriler arasındaki bu ilişkiyi belirlememize yardımcı olur ve bu sayede yeni verileri tahmin edebiliriz. 
 
-# In[1]:
 
 
 import pandas as pd
@@ -18,28 +17,12 @@ from sklearn import linear_model
 # veri setimizi import ediyoruz, ayraç olarak noktalı virgül olduğu için bunu belirtiyoruz:
 df = pd.read_csv("multilinearregression.csv",sep = ";")
 
-
-
-# In[2]:
-
-
 # Veri setimizi görelim ve doğru import ettiğiniz kontrol edelim:
 df
 
-
-# In[3]:
-
-
 df[['alan', 'odasayisi', 'binayasi']]
 
-
-# In[4]:
-
-
 df['fiyat']
-
-
-# In[5]:
 
 
 # linear regression modeli tanımlıyoruz:
@@ -52,37 +35,11 @@ reg.fit(df[['alan', 'odasayisi', 'binayasi']], df['fiyat'])
 reg.predict([[230,4,10]])
 
 
-# In[10]:
-
-
 reg.predict([[230,6,0]])
-
-
-# In[11]:
-
-
 reg.predict([[355,3,20]])
-
-
-# In[12]:
-
-
 reg.predict([[230,4,10], [230,6,0], [355,3,20]])
-
-
-# In[13]:
-
-
 reg.coef_
-
-
-# In[14]:
-
-
 reg.intercept_
-
-
-# In[15]:
 
 
 # Multiple Linear regression formülümüze dönersek hatırlayalım:
@@ -99,34 +56,5 @@ x3 = 10
 y = a + b1*x1 + b2*x2 + b3*x3
 
 y
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
